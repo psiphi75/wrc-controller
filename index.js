5d6592c7d0fa9d9dd5807bffda32a37de7bfa5ad
Simon Werner <simonwerner@gmail.com>
2016-07-21 17:00:18 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+/*********************************************************************
+ *                                                                   *
+ *   Copyright 2016 Simon M. Werner                                  *
+ *                                                                   *
+ *   Licensed to the Apache Software Foundation (ASF) under one      *
+ *   or more contributor license agreements.  See the NOTICE file    *
+ *   distributed with this work for additional information           *
+ *   regarding copyright ownership.  The ASF licenses this file      *
+ *   to you under the Apache License, Version 2.0 (the               *
+ *   'License'); you may not use this file except in compliance      *
+ *   with the License.  You may obtain a copy of the License at      *
+ *                                                                   *
+ *      http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                                   *
+ *   Unless required by applicable law or agreed to in writing,      *
+ *   software distributed under the License is distributed on an     *
+ *   'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY          *
+ *   KIND, either express or implied.  See the License for the       *
+ *   specific language governing permissions and limitations         *
+ *   under the License.                                              *
+ *                                                                   *
+ *********************************************************************/
+
+'use strict';
+
+var stat = require('node-static');
+
+function WRCController(port, log) {
+    port = port || 8888;
+    log = log || console.log;
+
+    var fileServer = new stat.Server('./public');
+
+    require('http').createServer(function (request, response) {
+        request.addListener('end', function () {
+            //
+            // Serve files!
+            //
+            fileServer.serve(request, response)
+            .addListener('error', function (err) {
+                log('Error serving ' + request.url + ' - ' + err.message);
+            })
+            .addListener('success', function (obj) {
+                log(request.url + ' (' + obj.status + ')', obj.headers.Date);
+            });
+        }).resume();
+
+    }).listen(port);
+
+    console.log('Point your mobile phone to http://[IP THIS COMPUTER]:' + port);
+}
+
+module.exports = WRCController;
